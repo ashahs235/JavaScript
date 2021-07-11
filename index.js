f0f11fc6c13b0c7f51fd354a4fa4df41c3ce3fb8
ashahs235 <65362458+ashahs235@users.noreply.github.com>
2021-07-11 21:32:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,143 @@
+console.log("Coding"); //not visible in browser, visible only in console
+console.warn("do not make errors in programs");
+console.error("Syntax errors");
+
+
+/*let subject = "Programming"; //changed only once, reusable, eg username on fb, insta / let varies
+console.log("super power is", subject);
+console.log("I am interested in", subject);
+
+const pi = "3.142"; //no need to redefine
+console.log("value of pi is", pi);
+
+let demand = "Java"; //change to const n show the error
+console.log("the demanding language in IT is", demand);
+
+demand = "Javascript"; //not stable, keeps changing, reassigning/redefining - don't write let keyword
+console.log("the current demanding language in IT is", demand);
+
+demand = "Python";
+console.log("the demanding language now in IT is", demand);
+
+console.log("Coding"); //not visible in browser, visible only in console
+console.warn("do not make errors in programs");
+console.error("Syntax errors"); 
+
+let language = "Javascript Coding";
+console.log(typeof language);
+
+const pi = 3.14; //"" string, number
+console.log(typeof pi);
+
+//special characters are also string
+let email = "asha@coding.com";
+console.log(typeof email);
+
+const isAdult = true;
+console.log(typeof isAdult);
+
+const isTeenager = false;
+console.log(typeof isTeenager);
+
+const age = 20; //10
+if (age > 18)
+{
+    console.log("Person is eligible for voting");
+}
+else
+{
+    console.log("Person is not eligible for voting");
+} 
+
+const isAnAdult = true; //change it to false
+if(isAnAdult)
+{
+    console.log("Person is an adult"); //true
+}
+else
+{
+    console.log("Person is not an adult"); //false
+} 
+
+const isChessAnOutdoorGame = false; //isSum
+console.log(typeof isChessAnOutdoorGame);
+
+const isCricketAnOutdoorGame = true;
+console.log(typeof isCricketAnOutdoorGame); //if lower case c - undefined
+
+let isTennisAnOutdoorGame; //undefined variable : empty var, declared but not assigned
+console.log(isTennisAnOutdoorGame); //undefined - variable in which value is not defined
+
+isTennisAnOutdoorGame = "no"
+console.log(isTennisAnOutdoorGame);
+
+let isBadmintonAnOutdoorGame = "yes"; 
+console.log(isBadmintonAnOutdoorGame);
+
+//console.log(IPL); //printing something which is not defined  
+
+//infinity
+let number = 1;
+console.log(number);
+number = -1/0; //+&-
+console.log(number);
+
+//null
+let nonExistingValue = null;
+console.log(nonExistingValue);
+
+let valueFromServer = null;
+if(valueFromServer)
+{
+    console.log("value exists");
+}
+else
+{
+    console.error("Cannot fetch value from Server");
+}
+
+valueFromServer = "Apple"; //redefine or reassign
+if(valueFromServer)
+{
+    console.log("value exists");
+}
+else
+{
+    console.error("Cannot fetch value from Server");
+}
+
+let ServerData = null;
+console.log(ServerData);
+ServerData = "Apple";
+console.log(ServerData);
+
+let order = null; //currently not present, may be present in future //"app" //checking availability
+if(order)
+{
+    console.log("Item shipped");
+}
+else
+{
+    console.log("Item isn't present currently, it will be delivered late");
+}
+
+//null is used to store a value which does not exist yet - order is not present now, will be there in future
+//check the status
+order = "saree"; //present is future - redefining
+if(order)
+{
+    console.log(order ,"is shipped");
+}
+else
+{
+    console.log("Item isn't present currently, it will be delivered late");
+} */
+
+
+//summary
+//primitive data types
+let name = "Dave";
+let age = 35;
+let isProgrammer = true;
+let shipped = undefined;
+let isPurchased = null;
